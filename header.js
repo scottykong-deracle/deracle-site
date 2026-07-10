02e292d99d5ab802f3408bdfa6bfb15df12195ae
scottykong-deracle <scottykong@gmail.com>
2026-07-10 09:52:51 +0900

    Create header.js

---
+++ header.js
@@ -0,0 +1,14 @@
+document.write(`
+<header>
+    <nav>
+        <div class="logo"><a href="index.html">dERACLE</a></div>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="ssd-solutions.html">SSD</a></li>
+            <li><a href="dram-modules.html">DRAM</a></li>
+            <li><a href="insights.html">Insights</a></li>
+            <li><a href="contact.html">Contact</a></li>
+        </ul>
+    </nav>
+</header>
+`);
